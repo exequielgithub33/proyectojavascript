215454ef1f0d3fc968e7f08f10dd9542733abb01
exequiel gimenez <omarexe23@gmail.com>
2022-11-15 21:35:11 -0300

    proyecto simulador interactivo II entrega

--- tarea.js
+++ tarea.js
@@ -1,12 +1,26 @@
 
-let dbusuario = "omarexe";
-let dbclave="123";
+const permisos = [
+    {usuario:"omarexe", permiso:"lectura"},
+    {usuario:"ramon" , permiso:"escritura"},
+    {usuario:"raulperez", permiso:"escritura"},
+    {usuario:"jaimito", permiso:"administraor"},
+
+
+]
+ 
 
 
 function ingreso() {
-    let usuarioIngresado = prompt(`Ingrese Nombre de Usuario`);
-    let contrasenaIngresada = prompt(`Ingrese Contraseña`);
-    if  (usuarioIngresado=="omarexe" && contrasenaIngresada=="123") {
+    let usuario = {
+        usuarioIngresado : null,
+        contrasenaIngresada : null,
+        permiso:null
+    }
+
+
+     usuario.usuarioIngresado = prompt(`Ingrese Nombre de Usuario`);
+     usuario.contrasenaIngresada = prompt(`Ingrese Contraseña`);
+    if  (usuario.usuarioIngresado=="omarexe" && usuario.contrasenaIngresada=="123") {
            return   true
 
     }
@@ -34,9 +48,11 @@ do {
 
 let h1resp =  document.getElementById("h1respuesta");
 if  (res == true) {
-    h1resp.innerHTML = "Bienvenido al sitio "
+    let tipopermiso = permisos.find(permiso =>permiso.usuario ==="omarexe")
+    h1resp.innerHTML = `Bienvenido al sitio : Usuario  ${tipopermiso.usuario} tiene permiso de :${tipopermiso.permiso}` 
 }
 else {
     h1resp.innerHTML = "ingresaste mal Usuario o Contraseña"
 }
 console.log(h1resp.innerHTML);
+
